39b57995b749a7c3babdc0901217c9ae611f6924
Derek <derek-thornton@hotmail.ca>
2023-01-12 02:06:58 -0500

    added styles on extra details

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import picture from '../assets/lighthouseMobile.png';
 
 function Portfolio() {
     return (
@@ -9,7 +10,17 @@ function Portfolio() {
             <Link to='/'><span style={{color: 'white'}}><FontAwesomeIcon className='fa-lg' icon={faArrowLeft}/></span></Link>
             <div className='sheet'>
                 <h5>Development Commentary: </h5>
-                
+                <p>
+                    The initial design had far more use of 'expensive' effects; however, the performance cost on mobile
+                    devices did not justify extra flare. The new and current iteration was designed with the mobile-user experience
+                    in mind, and was thus able to keep the audit scores quite high.
+                </p>
+                <div style={{display: 'flex', width:'100%', justifyContent: 'center'}}><img src={picture} alt='Lighthouse audit on mobile' style={{maxHeight: '30vmax', maxWidth: '30vmax'}}/></div>
+                <p>
+                    My main purpose when designing the general layout was based on the premise that I would likely have
+                    less than a minute to make an impression on an on-looker. I opted to keep the design simple and clean, and presenting
+                    the projects almost immediately. Additional detail would be accessible upon further navigation for those interested.
+                </p>
         
             </div>
             </div>
